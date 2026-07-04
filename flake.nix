c5eac6a13f19ead3b27e4c4fe33df0d0998337f5
Dataram57 <contact@dataram57.com>
2026-07-04 22:30:57 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "TS + tsx starter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+
+            if [ ! -d node_modules ]; then
+              npm install
+            fi
+          '';
+        };
+      });
+}
