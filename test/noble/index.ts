098096663a46e75f973bd52625002675762ca3a9
anon <@>
2026-07-30 20:19:05 +0200

    tests are written less personal and print info better

--- test/noble/index.ts
+++ test/noble/index.ts
@@ -1,43 +1,46 @@
 import {
     // SHA-2
-    slh_dsa_sha2_128s as noble_sha2_128s,
-    slh_dsa_sha2_128f as noble_sha2_128f,
-    slh_dsa_sha2_192s as noble_sha2_192s,
-    slh_dsa_sha2_192f as noble_sha2_192f,
-    slh_dsa_sha2_256s as noble_sha2_256s,
-    slh_dsa_sha2_256f as noble_sha2_256f,
+    slh_dsa_sha2_128s as other_sha2_128s,
+    slh_dsa_sha2_128f as other_sha2_128f,
+    slh_dsa_sha2_192s as other_sha2_192s,
+    slh_dsa_sha2_192f as other_sha2_192f,
+    slh_dsa_sha2_256s as other_sha2_256s,
+    slh_dsa_sha2_256f as other_sha2_256f,
 
     // SHAKE
-    slh_dsa_shake_128s as noble_shake_128s,
-    slh_dsa_shake_128f as noble_shake_128f,
-    slh_dsa_shake_192s as noble_shake_192s,
-    slh_dsa_shake_192f as noble_shake_192f,
-    slh_dsa_shake_256s as noble_shake_256s,
-    slh_dsa_shake_256f as noble_shake_256f,
+    slh_dsa_shake_128s as other_shake_128s,
+    slh_dsa_shake_128f as other_shake_128f,
+    slh_dsa_shake_192s as other_shake_192s,
+    slh_dsa_shake_192f as other_shake_192f,
+    slh_dsa_shake_256s as other_shake_256s,
+    slh_dsa_shake_256f as other_shake_256f,
 } from "@noble/post-quantum/slh-dsa.js";
 
 import {
     // SHA-2
-    slh_dsa_sha2_128s as dr57_sha2_128s,
-    slh_dsa_sha2_128f as dr57_sha2_128f,
-    slh_dsa_sha2_192s as dr57_sha2_192s,
-    slh_dsa_sha2_192f as dr57_sha2_192f,
-    slh_dsa_sha2_256s as dr57_sha2_256s,
-    slh_dsa_sha2_256f as dr57_sha2_256f,
+    slh_dsa_sha2_128s,
+    slh_dsa_sha2_128f,
+    slh_dsa_sha2_192s,
+    slh_dsa_sha2_192f,
+    slh_dsa_sha2_256s,
+    slh_dsa_sha2_256f,
 
     // SHAKE
-    slh_dsa_shake_128s as dr57_shake_128s,
-    slh_dsa_shake_128f as dr57_shake_128f,
-    slh_dsa_shake_192s as dr57_shake_192s,
-    slh_dsa_shake_192f as dr57_shake_192f,
-    slh_dsa_shake_256s as dr57_shake_256s,
-    slh_dsa_shake_256f as dr57_shake_256f,
+    slh_dsa_shake_128s,
+    slh_dsa_shake_128f,
+    slh_dsa_shake_192s,
+    slh_dsa_shake_192f,
+    slh_dsa_shake_256s,
+    slh_dsa_shake_256f
 } from "../../src/index.js";
 
 
 function stringToUint8Array(str: string): Uint8Array {
     return new TextEncoder().encode(str);
 }
+
+const THIS_NAME = "THIS_PACKAGE";
+const OTHER_NAME = "@NOBLE/POST-QUANTUM"
 
 const message = stringToUint8Array("Hello Sphincs");
 
@@ -49,89 +52,90 @@ function logTest(name: string, expect : boolean, result: boolean ){
     );
 }
 
-let dr57 : any;
-let noble : any;
+let thisSphincs : any;
+let otherSphincs : any;
 function testSignatureVerification(name : string, signature : Uint8Array, publicKey : Uint8Array, isSignatureCorrect : boolean){
     console.log(name);
     console.log("Signature length:", signature.length);
-    logTest("Signature - My verifier", isSignatureCorrect, dr57.verify(message, publicKey, signature));
-    logTest("Signature - Noble verifier", isSignatureCorrect, noble.verify(signature, message, publicKey));
+    logTest(`Signature - ${THIS_NAME} verifier`, isSignatureCorrect, thisSphincs.verify(message, publicKey, signature));
+    logTest(`Signature - ${OTHER_NAME} verifier`, isSignatureCorrect, otherSphincs.verify(signature, message, publicKey));
 }
 
 
 
-function testScheme(title : string, _dr57 : any, _noble : any){
+function testScheme(title : string, _thisSphincs : any, _otherSphincs : any){
     console.log("Testing:", title);
-    dr57 = _dr57;
-    noble = _noble;
+    thisSphincs = _thisSphincs;
+    otherSphincs = _otherSphincs;
 
     //My key pair
-    const keyPair = dr57.keygen();
+    const keyPair = thisSphincs.keygen();
 
     testSignatureVerification(
-        "Veryfing MY signature signed by MY keys",
-        dr57.sign(message, keyPair.secretKey),
+        `Veryfing ${THIS_NAME} signature signed by ${THIS_NAME} keys`,
+        thisSphincs.sign(message, keyPair.secretKey),
         keyPair.publicKey,
         true
     );
     testSignatureVerification(
-        "Veryfing NOBLE signature signed by MY keys",
-        noble.sign(message, keyPair.secretKey),
+        `Veryfing ${OTHER_NAME} signature signed by ${THIS_NAME} keys`,
+        otherSphincs.sign(message, keyPair.secretKey),
         keyPair.publicKey,
         true
     );
-    //Noble key pair
-    const nobleKeyPair = noble.keygen();
+    //Other key pair
+    const otherKeyPair = otherSphincs.keygen();
     testSignatureVerification(
-        "Veryfing MY signature signed by NOBLE keys",
-        dr57.sign(message, nobleKeyPair.secretKey),
-        nobleKeyPair.publicKey,
+        `Veryfing ${THIS_NAME} signature signed by ${OTHER_NAME} keys`,
+        thisSphincs.sign(message, otherKeyPair.secretKey),
+        otherKeyPair.publicKey,
         true
     );
     testSignatureVerification(
-        "Veryfing NOBLE signature signed by NOBLE keys",
-        noble.sign(message, nobleKeyPair.secretKey),
-        nobleKeyPair.publicKey,
+        `Veryfing ${OTHER_NAME} signature signed by ${OTHER_NAME} keys`,
+        otherSphincs.sign(message, otherKeyPair.secretKey),
+        otherKeyPair.publicKey,
         true
     );
     //Wrong public key
     testSignatureVerification(
-        "Veryfing MY signature signed by MY keys against NOBLE wrong keys",
-        dr57.sign(message, keyPair.secretKey),
-        nobleKeyPair.publicKey,
+        `Veryfing ${THIS_NAME} signature signed by ${THIS_NAME} keys against ${OTHER_NAME} wrong keys`,
+        thisSphincs.sign(message, keyPair.secretKey),
+        otherKeyPair.publicKey,
         false
     );
     testSignatureVerification(
-        "Veryfing NOBLE signature signed by MY keys against NOBLE wrong keys",
-        noble.sign(message, keyPair.secretKey),
-        nobleKeyPair.publicKey,
+        `Veryfing ${OTHER_NAME} signature signed by ${THIS_NAME} keys against ${OTHER_NAME} wrong keys`,
+        otherSphincs.sign(message, keyPair.secretKey),
+        otherKeyPair.publicKey,
         false
     );
-    //Noble Wrong public key
+    //Other Wrong public key
     testSignatureVerification(
-        "Veryfing MY signature signed by NOBLE keys against MY wrong keys",
-        dr57.sign(message, nobleKeyPair.secretKey),
+        `Veryfing ${THIS_NAME} signature signed by ${OTHER_NAME} keys against ${THIS_NAME} wrong keys`,
+        thisSphincs.sign(message, otherKeyPair.secretKey),
         keyPair.publicKey,
         false
     );
     testSignatureVerification(
-        "Veryfing NOBLE signature signed by NOBLE keys against MY wrong keys",
-        noble.sign(message, nobleKeyPair.secretKey),
+        `Veryfing ${OTHER_NAME} signature signed by ${OTHER_NAME} keys against ${THIS_NAME} wrong keys`,
+        otherSphincs.sign(message, otherKeyPair.secretKey),
         keyPair.publicKey,
         false
     );
 }
 
-testScheme("SHA2-128s", dr57_sha2_128s, noble_sha2_128s);
-testScheme("SHA2-128f", dr57_sha2_128f, noble_sha2_128f);
-testScheme("SHA2-192s", dr57_sha2_192s, noble_sha2_192s);
-testScheme("SHA2-192f", dr57_sha2_192f, noble_sha2_192f);
-testScheme("SHA2-256s", dr57_sha2_256s, noble_sha2_256s);
-testScheme("SHA2-256f", dr57_sha2_256f, noble_sha2_256f);
 
-testScheme("SHAKE-128s", dr57_shake_128s, noble_shake_128s);
-testScheme("SHAKE-128f", dr57_shake_128f, noble_shake_128f);
-testScheme("SHAKE-192s", dr57_shake_192s, noble_shake_192s);
-testScheme("SHAKE-192f", dr57_shake_192f, noble_shake_192f);
-testScheme("SHAKE-256s", dr57_shake_256s, noble_shake_256s);
-testScheme("SHAKE-256f", dr57_shake_256f, noble_shake_256f);
+testScheme("SHA2-128s", slh_dsa_sha2_128s, other_sha2_128s);
+testScheme("SHA2-128f", slh_dsa_sha2_128f, other_sha2_128f);
+testScheme("SHA2-192s", slh_dsa_sha2_192s, other_sha2_192s);
+testScheme("SHA2-192f", slh_dsa_sha2_192f, other_sha2_192f);
+testScheme("SHA2-256s", slh_dsa_sha2_256s, other_sha2_256s);
+testScheme("SHA2-256f", slh_dsa_sha2_256f, other_sha2_256f);
+
+testScheme("SHAKE-128s", slh_dsa_shake_128s, other_shake_128s);
+testScheme("SHAKE-128f", slh_dsa_shake_128f, other_shake_128f);
+testScheme("SHAKE-192s", slh_dsa_shake_192s, other_shake_192s);
+testScheme("SHAKE-192f", slh_dsa_shake_192f, other_shake_192f);
+testScheme("SHAKE-256s", slh_dsa_shake_256s, other_shake_256s);
+testScheme("SHAKE-256f", slh_dsa_shake_256f, other_shake_256f);
--- test/sphincsplus/index.ts
+++ test/sphincsplus/index.ts
@@ -21,20 +21,20 @@ import {
 
 import {
     // SHA-2
-    slh_dsa_sha2_128s as dr57_sha2_128s,
-    slh_dsa_sha2_128f as dr57_sha2_128f,
-    slh_dsa_sha2_192s as dr57_sha2_192s,
-    slh_dsa_sha2_192f as dr57_sha2_192f,
-    slh_dsa_sha2_256s as dr57_sha2_256s,
-    slh_dsa_sha2_256f as dr57_sha2_256f,
+    slh_dsa_sha2_128s,
+    slh_dsa_sha2_128f,
+    slh_dsa_sha2_192s,
+    slh_dsa_sha2_192f,
+    slh_dsa_sha2_256s,
+    slh_dsa_sha2_256f,
 
     // SHAKE
-    slh_dsa_shake_128s as dr57_shake_128s,
-    slh_dsa_shake_128f as dr57_shake_128f,
-    slh_dsa_shake_192s as dr57_shake_192s,
-    slh_dsa_shake_192f as dr57_shake_192f,
-    slh_dsa_shake_256s as dr57_shake_256s,
-    slh_dsa_shake_256f as dr57_shake_256f,
+    slh_dsa_shake_128s,
+    slh_dsa_shake_128f,
+    slh_dsa_shake_192s,
+    slh_dsa_shake_192f,
+    slh_dsa_shake_256s,
+    slh_dsa_shake_256f
 } from "../../src/index.js";
 
 
@@ -54,90 +54,90 @@ function logTest(name: string, expect : boolean, result: boolean ){
     );
 }
 
-let dr57 : any;
-let other : any;
+let thisSphincs : any;
+let otherSphincs : any;
 function testSignatureVerification(name : string, signature : Uint8Array, publicKey : Uint8Array, isSignatureCorrect : boolean){
     console.log(name);
     console.log("Signature length:", signature.length);
-    logTest(`Signature - ${THIS_NAME} verifier`, isSignatureCorrect, dr57.verify(message, publicKey, signature));
-    logTest(`Signature - ${OTHER_NAME} verifier`, isSignatureCorrect, other.verify(message, publicKey, signature));
+    logTest(`Signature - ${THIS_NAME} verifier`, isSignatureCorrect, thisSphincs.verify(message, publicKey, signature));
+    logTest(`Signature - ${OTHER_NAME} verifier`, isSignatureCorrect, otherSphincs.verify(message, publicKey, signature));
 }
 
 
 
-function testScheme(title : string, _dr57 : any, _other : any){
+function testScheme(title : string, _thisSphincs : any, _otherSphincs : any){
     console.log("Testing:", title);
-    dr57 = _dr57;
-    other = _other;
+    thisSphincs = _thisSphincs;
+    otherSphincs = _otherSphincs;
 
     //My key pair
-    const keyPair = dr57.keygen();
+    const keyPair = thisSphincs.keygen();
 
     testSignatureVerification(
         `Veryfing ${THIS_NAME} signature signed by ${THIS_NAME} keys`,
-        dr57.sign(message, keyPair.secretKey),
+        thisSphincs.sign(message, keyPair.secretKey),
         keyPair.publicKey,
         true
     );
     testSignatureVerification(
         `Veryfing ${OTHER_NAME} signature signed by ${THIS_NAME} keys`,
-        other.sign(message, keyPair.secretKey),
+        otherSphincs.sign(message, keyPair.secretKey),
         keyPair.publicKey,
         true
     );
     //Other key pair
-    const otherKeyPair = other.keygen();
+    const otherKeyPair = otherSphincs.keygen();
     testSignatureVerification(
         `Veryfing ${THIS_NAME} signature signed by ${OTHER_NAME} keys`,
-        dr57.sign(message, otherKeyPair.secretKey),
+        thisSphincs.sign(message, otherKeyPair.secretKey),
         otherKeyPair.publicKey,
         true
     );
     testSignatureVerification(
         `Veryfing ${OTHER_NAME} signature signed by ${OTHER_NAME} keys`,
-        other.sign(message, otherKeyPair.secretKey),
+        otherSphincs.sign(message, otherKeyPair.secretKey),
         otherKeyPair.publicKey,
         true
     );
     //Wrong public key
     testSignatureVerification(
         `Veryfing ${THIS_NAME} signature signed by ${THIS_NAME} keys against ${OTHER_NAME} wrong keys`,
-        dr57.sign(message, keyPair.secretKey),
+        thisSphincs.sign(message, keyPair.secretKey),
         otherKeyPair.publicKey,
         false
     );
     testSignatureVerification(
         `Veryfing ${OTHER_NAME} signature signed by ${THIS_NAME} keys against ${OTHER_NAME} wrong keys`,
-        other.sign(message, keyPair.secretKey),
+        otherSphincs.sign(message, keyPair.secretKey),
         otherKeyPair.publicKey,
         false
     );
     //Other Wrong public key
     testSignatureVerification(
         `Veryfing ${THIS_NAME} signature signed by ${OTHER_NAME} keys against ${THIS_NAME} wrong keys`,
-        dr57.sign(message, otherKeyPair.secretKey),
+        thisSphincs.sign(message, otherKeyPair.secretKey),
         keyPair.publicKey,
         false
     );
     testSignatureVerification(
         `Veryfing ${OTHER_NAME} signature signed by ${OTHER_NAME} keys against ${THIS_NAME} wrong keys`,
-        other.sign(message, otherKeyPair.secretKey),
+        otherSphincs.sign(message, otherKeyPair.secretKey),
         keyPair.publicKey,
         false
     );
 }
 
 
-testScheme("SHA2-128s", dr57_sha2_128s, other_sha2_128s);
-testScheme("SHA2-128f", dr57_sha2_128f, other_sha2_128f);
-testScheme("SHA2-192s", dr57_sha2_192s, other_sha2_192s);
-testScheme("SHA2-192f", dr57_sha2_192f, other_sha2_192f);
-testScheme("SHA2-256s", dr57_sha2_256s, other_sha2_256s);
-testScheme("SHA2-256f", dr57_sha2_256f, other_sha2_256f);
+testScheme("SHA2-128s", slh_dsa_sha2_128s, other_sha2_128s);
+testScheme("SHA2-128f", slh_dsa_sha2_128f, other_sha2_128f);
+testScheme("SHA2-192s", slh_dsa_sha2_192s, other_sha2_192s);
+testScheme("SHA2-192f", slh_dsa_sha2_192f, other_sha2_192f);
+testScheme("SHA2-256s", slh_dsa_sha2_256s, other_sha2_256s);
+testScheme("SHA2-256f", slh_dsa_sha2_256f, other_sha2_256f);
 
-testScheme("SHAKE-128s", dr57_shake_128s, other_shake_128s);
-testScheme("SHAKE-128f", dr57_shake_128f, other_shake_128f);
-testScheme("SHAKE-192s", dr57_shake_192s, other_shake_192s);
-testScheme("SHAKE-192f", dr57_shake_192f, other_shake_192f);
-testScheme("SHAKE-256s", dr57_shake_256s, other_shake_256s);
-testScheme("SHAKE-256f", dr57_shake_256f, other_shake_256f);
+testScheme("SHAKE-128s", slh_dsa_shake_128s, other_shake_128s);
+testScheme("SHAKE-128f", slh_dsa_shake_128f, other_shake_128f);
+testScheme("SHAKE-192s", slh_dsa_shake_192s, other_shake_192s);
+testScheme("SHAKE-192f", slh_dsa_shake_192f, other_shake_192f);
+testScheme("SHAKE-256s", slh_dsa_shake_256s, other_shake_256s);
+testScheme("SHAKE-256f", slh_dsa_shake_256f, other_shake_256f);
